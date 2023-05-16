e97de51075220e9ae3899ec265e5054614e5d548
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-16 19:20:10 +0530

    Update WhatsNew.jsx

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -34,7 +34,7 @@ const WhatsNew = () => (
         className={`flex-1 ${styles.flexCenter}`}
       >
         <img
-          src="/0.png"
+          src="/1.png"
           alt="get-started"
           className="w-[90%] h-[90%] object-contain"
         />
