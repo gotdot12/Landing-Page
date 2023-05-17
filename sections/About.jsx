8527405deacbf3004a35db16dcbe26f51859a73e
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-17 09:27:10 +0530

    Update About.jsx

--- sections/About.jsx
+++ sections/About.jsx
@@ -7,10 +7,10 @@ import styles from '../styles';
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <div className="gradient-02 z-0" />
-    <div>
+    <div className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}>
       <TypingText title="| About GODOT" textStyles="text-center" />
 
-      <p>
+      <p className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white">
         <span className="font-extrabold text-white">GODOT</span> token is a cryptocurrency token that powers the
         GodotNetwork DeFi Ecosystem. The automated buyback function
         strenthens{' '} the{' '}
