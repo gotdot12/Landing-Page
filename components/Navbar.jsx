455dec08f1282d0143fe9cb3d9c3aa0e2587877b
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-17 09:37:35 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,9 @@
 'use client';
 
-import { motion } from 'framer-motion';
-
 import styles from '../styles';
-import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
-  <motion.nav
-    variants={navVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
+  <nav className={`${styles.xPaddings} py-8 relative`}>
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
@@ -30,7 +22,7 @@ const Navbar = () => (
         style={{ visibility: 'hidden' }}
       />
     </div>
-  </motion.nav>
+  </nav>
 );
 
 export default Navbar;
