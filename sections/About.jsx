7f73cdd847efb03350c5a06e35eaa4d02b8d2dae
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-17 09:23:29 +0530

    Update About.jsx

--- sections/About.jsx
+++ sections/About.jsx
@@ -22,7 +22,6 @@ const About = () => (
       </p>
 
       <img
-        variants={fadeIn('up', 'tween', 0.3, 1)}
         src="/arrow-down.svg"
         alt="arrow down"
         className="w-[18px] h-[28px] object-contain mt-[28px]"
