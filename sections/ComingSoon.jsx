eb34b7bb84f92b03e944b1fc5ea68bea59a0b71f
gotdot12 <129498729+gotdot12@users.noreply.github.com>
2023-05-17 09:24:56 +0530

    Update ComingSoon.jsx

--- sections/ComingSoon.jsx
+++ sections/ComingSoon.jsx
@@ -1,21 +1,12 @@
 'use client';
 
-import { motion } from 'framer-motion';
 
 import styles from '../styles';
 import { TitleText, TypingText } from '../components';
-import { fadeIn, staggerContainer } from '../utils/motion';
 
 const ComingSoon = () => (
   <section className={`${styles.paddings} relative z-10`}>
-    <motion.div
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex flex-col`}
-    >
-
+    <div>
       <TypingText title="| Upcoming Updates" textStyles="text-center" />
       <TitleText
         title={(
@@ -23,7 +14,7 @@ const ComingSoon = () => (
         )}
         textStyles="text-center"
       />
-    </motion.div>
+    </div>
   </section>
 );
 
